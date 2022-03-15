7fe1ee782e51d62f08d4fd63d3cf85c3e1b6737a
Evan Chernicky <echernicky@gmail.com>
2022-03-15 19:35:02 -0400

    commit tree 3270af75ed15f86a41c39fc95d4f733ad377ad8b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_reader :matches
+    def initialize(word)
+        @word = word
+    end
+    def match(matches)
+        if (@word != @matches)
+            []
+        end
+    end
+end
